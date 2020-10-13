fa64ce6d13faeeaac24a09e7cb818abc05bb2a3c
Cyberboa <72196905+Cyberboa@users.noreply.github.com>
2020-10-13 16:12:28 +0200

    - test exercises

--- quizGameApp/views.py
+++ quizGameApp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import View, TemplateView
 
 
 # Create your views here.
 class HomeView(View):
     def get(httprequest, *args):
-        return render(httprequest, "home.html", {})
+        my_dict = {
+            "name": "roshan",
+            "lastname": "hausammann",
+            "year": 1922,
+        }
+        return render(httprequest, "home.html", my_dict)
