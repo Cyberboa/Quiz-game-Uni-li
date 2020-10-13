10ba0dc1a102c7c1c0b7bf06188e70afea16a572
Cyberboa <72196905+Cyberboa@users.noreply.github.com>
2020-10-13 13:38:14 +0200

    - Creating DB Table Question

--- quizGameSystem/quizGameApp/models.py
+++ quizGameSystem/quizGameApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    corransw = models.CharField(max_length=100)
