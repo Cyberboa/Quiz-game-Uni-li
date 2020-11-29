3764d0308342ea7955983a66b4cb58f4af3c397f
claudi13 <claudia.schedler@uni.li>
2020-11-29 19:22:44 +0100

    - tried to add a audio file- maybe we need to remove it and better do it in the html - also tried it out in html-the player can be hidden

---
+++ PlayAudio.py
@@ -0,0 +1,6 @@
+import vlc
+import time
+
+sound_file = vlc.MediaPlayer(r"C:\Users\ClaSch01\Desktop\UNI LIE\Information System Developement\Quiz-game-Uni-li\quizGameApp\static\IntroMusik.mp3")
+sound_file.play()
+time.sleep(50)
