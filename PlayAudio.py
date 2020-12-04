016364e8976d94972768b8c3b52a09f759f01ef1
claudi13 <claudia.schedler@uni.li>
2020-12-04 20:04:26 +0100

    - changed picture - fixed audio player

--- PlayAudio.py
+++
@@ -1,6 +0,0 @@
-import vlc
-import time
-
-sound_file = vlc.MediaPlayer(r"C:\Users\ClaSch01\Desktop\UNI LIE\Information System Developement\Quiz-game-Uni-li\quizGameApp\static\IntroMusik.mp3")
-sound_file.play()
-time.sleep(50)
